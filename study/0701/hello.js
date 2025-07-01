9c39a5e5dffb2a02d9f6cd519ff7da5d3a2c1fda
leejaehee1 <54349213+leejaehee1@users.noreply.github.com>
2025-07-01 16:13:20 +0900

    타입스크립트 기본 자료형 연습

--- study/0701/hello.js
+++ study/0701/hello.js
@@ -1,2 +1,22 @@
-"use strict";
 console.log("Hello World!!!");
+function greet(name) {
+    return "Hello, ".concat(name);
+}
+console.log(greet("John"));
+var n = 1;
+var b = true;
+var s = "Hello";
+var o = {};
+var n1 = 1;
+var b1 = true;
+var s1 = "Hello";
+var o1 = {};
+var a = 0;
+a = "Hello";
+a = true;
+a = {};
+var u = undefined;
+// u = 1;
+var count = 10, message = "Your count";
+var result = "".concat(message, " is ").concat(count);
+console.log(result);

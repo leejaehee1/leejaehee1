4839d5f1d5aab7b6edb6d149a5b840c143cc2fb4
leejaehee1 <54349213+leejaehee1@users.noreply.github.com>
2025-07-14 15:02:11 +0900

    수업내용 반영

---
+++ study/0714/ch03_4/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import DirectionTest from "./pages/DirectionTest";
+import JustifyCenterTest from "./pages/JustifyCenterTest";
+import UserContainer from "./pages/UserContainer";
+import CardContainer from "./pages/CardContainer";
+
+export default function App() {
+  return (
+    <main>
+      {/* <DirectionTest /> */}
+      {/* <JustifyCenterTest /> */}
+      {/* <UserContainer /> */}
+      <CardContainer />
+    </main>
+  );
+}

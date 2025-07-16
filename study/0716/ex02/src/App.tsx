c47e119c4afcdf9b5547d5a2faa2ebf51aba2720
leejaehee1 <54349213+leejaehee1@users.noreply.github.com>
2025-07-16 12:51:12 +0900

    폐차 진행 연습문제

---
+++ study/0716/ex02/src/App.tsx
@@ -0,0 +1,13 @@
+import GreetingPage from "./pages/GreetingPage";
+import ApplyScrapPage from "./pages/ApplyScrapPage";
+
+function App() {
+  return (
+    <div>
+      {/* <GreetingPage />; */}
+      <ApplyScrapPage />
+    </div>
+  );
+}
+
+export default App;
